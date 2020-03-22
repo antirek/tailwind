18c674adfbbae019abf73af0349f5a3abec1994b
sergey <serge.dmitriev@gmail.com>
2020-03-23 00:53:31 +0700

    fix

--- cards/index.js
+++ cards/index.js
@@ -1,9 +1,14 @@
 const pug = require('pug');
 
-
 const visitcard = {
     render: (ele) => {
         return pug.renderFile(__dirname + '/visitcard.pug', ele);
+    }
+}
+
+const buttons = {
+    render: (ele) => {
+        return pug.renderFile(__dirname + '/buttons.pug', ele);
     }
 }
 
@@ -12,7 +17,7 @@ const elements = [
         type: 'meta',
         title: 'Hello page',
         author: 'Bregan',
-        background: 'http://wewrwer.ru/dsfdsf.jpg'
+        background: 'http://wewrwer.ru/dsfdsf.jpg',
     },
     {
         type: 'visitcard',
@@ -25,9 +30,32 @@ const elements = [
             email: 'sddfkdfl;gk@bnbn.ru',
         },
     },
+    {
+        type: 'buttonplace',
+        style: 'icons',
+        buttons: [
+            {
+                id: '',
+                text: 'Напиши в WhatsApp',
+                type: 'whatsapp',
+                number: '7836584658'
+            },
+            {
+                id: '',
+                text: 'Viber',
+                type: 'viber',
+                number: '7836584658'
+            },
+        ],
+    },
 ];
 
-const html = () => visitcard.render(elements[1]);
+const html = () => {
+    let ht = [];
+    ht.push(visitcard.render(elements[1]));
+    ht.push(buttons.render(elements[2]));
+    return ht.join("");
+};
 
 module.exports = {
     html,
