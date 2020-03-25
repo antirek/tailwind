0687ed1d6d5e2dac51180a15825f94897fac8191
sergey <serge.dmitriev@gmail.com>
2020-03-26 01:27:44 +0700

    fix

--- cards/index.js
+++ cards/index.js
@@ -18,78 +18,22 @@ const questions = {
     }
 }
 
-const elements = [
-    {
-        type: 'meta',
-        title: 'Hello page',
-        author: 'Bregan',
-        background: 'http://wewrwer.ru/dsfdsf.jpg',
-    },
-    {
-        type: 'visitcard',
-        photo: 'http://',
-        name: 'Allow',
-        description: 'My head is egg.\nDraw youserlf.',
-        contacts: {
-            phone: '78978974359',
-            phone2: null,
-            phone3: null,
-            email: 'sddfkdfl;gk@bnbn.ru',
-        },
-    },
-    {
-        type: 'buttons',
-        style: 'icons',
-        buttons: [
-            {
-                id: '',
-                text: 'Напиши в WhatsApp',
-                icon: 'whatsapp',
-                url: 'http://www.mobilon.ru/',
-                bgColor: 'bg-green-500',
-                bgHoverColor: 'bg-green-400',
-                textColor: 'text-white',
-            },
-            {
-                id: '',
-                text: 'Viber',
-                icon: 'viber',
-                url: '7836584658',
-                bgColor: 'bg-purple-800',
-                bgHoverColor: 'bg-purple-600',
-                textColor: 'text-white',
-            },
-            {
-                id: '',
-                text: 'Telegram',
-                icon: 'viber',
-                url: '7836584658',
-                bgColor: 'bg-blue-500',
-                bgHoverColor: 'bg-blue-400',
-                textColor: 'text-white',
-            },
-        ],
-    },
-    {
-        type: 'faq',
-        style: 'compact',
-        questions: [
-            {
-                question: 'How do you do?',
-                answer: 'All right!',
-            },
-            {
-                question: 'How do you do?',
-                answer: 'All right!',
-            },
-        ],
-    },
-];
 
-const html = () => {
+const renderers = {
+    visitcard,
+    buttons,
+}
+
+const html = (elements) => {
     let ht = [];
-    ht.push(visitcard.render(elements[1]));
-    ht.push(buttons.render(elements[2]));
+    for (let el of elements) {
+        const type = el.type;
+        if (renderers[type]) {
+            ht.push(renderers[type].render(el));
+        }
+    }
+    // ht.push(visitcard.render(elements[1]));
+    // ht.push(buttons.render(elements[2]));
     return ht.join("");
 };
 
--- index.js
+++ index.js
@@ -10,7 +10,75 @@ app.set('view engine', 'pug')
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/index', function (req, res) {
-    res.render('index', { data: cards.html()})
+  const elements = [
+    {
+      type: 'meta',
+      title: 'Hello page',
+      author: 'Bregan',
+      background: 'http://wewrwer.ru/dsfdsf.jpg',
+    },
+    {
+      type: 'visitcard',
+      photo: 'http://',
+      name: 'Allow',
+      description: 'My head is egg.\nDraw youserlf.',
+      contacts: {
+        phone: '78978974359',
+        phone2: null,
+        phone3: null,
+        email: 'sddfkdfl;gk@bnbn.ru',
+      },
+    },
+    {
+      type: 'buttons',
+      style: 'icons',
+      buttons: [
+        {
+          id: '',
+          text: 'Напиши в WhatsApp',
+          icon: 'whatsapp',
+          url: 'http://www.mobilon.ru/',
+          bgColor: 'bg-green-500',
+          bgHoverColor: 'bg-green-400',
+          textColor: 'text-white',
+        },
+        {
+          id: '',
+          text: 'Viber',
+          icon: 'viber',
+          url: '7836584658',
+          bgColor: 'bg-purple-800',
+          bgHoverColor: 'bg-purple-600',
+          textColor: 'text-white',
+        },
+        {
+          id: '',
+          text: 'Telegram',
+          icon: 'viber',
+          url: '7836584658',
+          bgColor: 'bg-blue-500',
+          bgHoverColor: 'bg-blue-400',
+          textColor: 'text-white',
+        },
+      ],
+    },
+    {
+      type: 'faq',
+      style: 'compact',
+      questions: [
+        {
+          question: 'How do you do?',
+          answer: 'All right!',
+        },
+        {
+          question: 'How do you do?',
+          answer: 'All right!',
+        },
+      ],
+    },
+  ];
+
+  res.render('index', { data: cards.html(elements)});
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
